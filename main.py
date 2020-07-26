a7ec3dfccc52775df18f24f9a3be0866a0c9e5ba
RickyChan21 <rickychan21@hotmail.com>
2020-07-26 13:36:56 -0500

    date format

--- main.py
+++ main.py
@@ -2,18 +2,20 @@ import pandas as pd
 import plotly.express as px
 
 data = pd.read_csv('daily.csv')
-data_cases = data[['date', 'state', 'positive']]
+data['date'] = pd.to_datetime(data['date'], format = '%Y%m%d').astype(str)
+data_cases = data[['date','state','positive']]
 
-fig = px.choropleth(data_cases,
-                     locationmode = "USA-states",
-                     locations = 'state',
-                     scope = "usa",                            
-                     color = "positive",                     # identify representing column
-                     hover_name = "state",
-                     animation_frame = "date",              # identify hover name        # identify date column        # select projection
-                     color_continuous_scale = 'YlGnBu',  # select prefer color scale
-                     range_color = [0, 500000]              # select range of dataset
+fig = px.choropleth(data_cases,                                    # dataframe de los casos
+                     locationmode = "USA-states",                  # mapa
+                     locations = 'state',                          # division por estados
+                     scope = "usa",                                # proyeccion                        
+                     color = "positive",                           # columna a graficar (casos positivos)                   
+                     hover_name = "state",                         # informacion al hacer hover
+                     animation_frame = "date",                     # columna a animar (fechas)
+                     color_continuous_scale = 'YlGnBu',            # escala de colores
+                     range_color = [0, 500000],                    # rango de los datos a graficar
+                     title = "Casos de COVID-19 en Estados Unidos" # titulo
                      )  
-                           
+
 fig.show()          
 fig.write_html("map.html")
